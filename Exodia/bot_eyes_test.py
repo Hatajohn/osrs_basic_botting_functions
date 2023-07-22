a592aeb0e9bbe61dacb24a294b6c5f0aa7197ca1
John Hata <hatajohn@gmail.com>
2023-07-22 10:56:34 -0400

    Updated bot_eyes_test to run tests for locate_cluster
    desc

--- Exodia/bot_eyes_test.py
+++ Exodia/bot_eyes_test.py
@@ -11,19 +11,15 @@ if __name__ == "__main__":
     # Initialize bot objects
     [bot_b, bot_e, bot_a] = Actions.bot_init(False)
 
-    fs = Env.screen_image()
-    cv2.rectangle(fs, bot_e.inventory_global, color=(0, 255, 0), thickness=2)
-    Env.debug_view(fs)
-    Env.debug_view(bot_e.curr_inventory)
-
     bot_e.force_debug(True)
 
     cyan = [([180, 180, 0], [255, 255, 40])]
     yellow = [([0, 230, 230], [30, 255, 255])]
+    purple = [([240, 0, 160], [255, 0, 200])]
 
     # Color order is BGR for some reason
     # point = bot_e.locate_color(bot_e.curr_client, boundaries=cyan, multi=True)
-    point = bot_e.locate_cluster(bot_e.curr_client, boundaries=cyan)
+    point = bot_e.locate_cluster(bot_e.curr_client, boundaries=purple, draw_contours=False, draw_clusters=False, draw_lines=True)
     temp_client = copy.deepcopy(bot_e.curr_client)
     temp_client = cv2.circle(temp_client, point[0], 10, color=(0, 0, 255), thickness=3)
     Env.debug_view(temp_client)
